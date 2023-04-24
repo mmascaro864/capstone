92be4cf15f7145e1b3799d2a57f369f27994c5e3
mmascaro864 <marc.j.mascaro@gmail.com>
2023-04-24 10:42:19 -0400

    initial commit

---
+++ recommendations.py
@@ -0,0 +1,63 @@
+# import necessary libraries
+import pandas as pd
+import numpy as np
+
+def create_ranked_offers(df):
+    '''
+    create_ranked_offers:
+        - determine top ranked offers based on offers_completed vs. offers_received
+    
+    INPUT:
+        - df: dataframe containing offers data
+       
+    OUTPUT:
+        - ranked_completed: the ranked completed offers
+    '''
+    
+    offers = df.groupby('offer_id').sum().reset_index()
+    
+    ranked_completed = offers.loc[:,['offer_id','offer_received', 'offer_completed']]
+    ranked_completed['completion_ratio'] = ranked_completed['offer_completed'] / ranked_completed['offer_received']
+    
+    ranked_completed = ranked_completed.sort_values(['completion_ratio'], ascending = False)
+    
+    return ranked_completed
+
+def popular_recommendations(ranked_completed, n_top):
+    '''
+    popular_recommendations:
+        - return a ranked list
+    INPUT:
+    ranked_completed - a dataframe of the ranked offer ids representing offers received and completed
+    n_top - an integer of the number recommendations you want back 
+
+    OUTPUT:
+    top_offers - a list of the n_top recommended offers
+    '''
+    # set max_offers equal to number of rows
+    max_offers = ranked_completed.shape[0]
+  
+    if n_top <= max_offers:
+        top_offers = list(ranked_completed['offer_id'][:n_top])
+        print('The top ', n_top, ' offer recommendations: ')
+    else:
+        return print('Please enter a value less than or equal to {}'.format(max_offers))
+
+    return top_offers
+
+def create_user_item_matrix(df):
+    '''
+    INPUT:
+    df - pandas dataframe with customer_id, offer_id, and offer_completed columns
+    
+    OUTPUT:
+    user_item - user item matrix 
+    
+    Description:
+    Return a matrix with customer ids as rows and offer ids on the columns with 1 values where a user interacted with 
+    an offer and a 0 otherwise
+    '''
+    user_item = df.groupby(['customer_id', 'offer_id'])['offer_completed'].max().unstack()
+    user_item = user_item.notnull().astype(int)
+    
+    return user_item # return the user_item matrix 
---
+++ wrangle_data.py
@@ -0,0 +1,213 @@
+# import libraries
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MultiLabelBinarizer
+
+def clean_portfolio():
+    '''
+    clean_portfolio: 
+        - prepares and cleans the Starbucks offer portfolio data for further processing
+
+    In: 
+        - None
+
+    Out: portfolio
+        - cleaned portfolio pandas dataframe
+    '''
+    # read in portfolio json data
+    portfolio = pd.read_json('data/portfolio.json', orient='records', lines=True)
+
+    # update column names
+    portfolio.rename(columns={'id' : 'offer_id', 'duration' : 'days_open', 'difficulty' : 'min_spend'},
+                      inplace = True)
+    
+    # initialize MultiLabelBinarizer to one-hot encode channels column
+    mlb = MultiLabelBinarizer()
+    mlb.fit(portfolio['channels'])
+    channels_temp = pd.DataFrame(mlb.transform(portfolio['channels']), columns = mlb.classes_)
+
+    # use pandas get_dummies to one-hot encode offer_type column
+    offer_type_temp = pd.get_dummies(portfolio['offer_type'])
+
+    # merge resultant dataframes back into portfolio
+    portfolio = pd.concat([portfolio, channels_temp, offer_type_temp], axis = 1)
+
+    # drop channels and offer_type columns
+    portfolio.drop(['channels', 'offer_type'], axis = 1, inplace = True)
+
+    # reorder columns
+    col_order = ['offer_id', 'reward', 'min_spend', 'days_open', 'email', 
+                'mobile', 'social', 'web', 'bogo', 'discount', 'informational']
+    
+    return portfolio.loc[:, col_order]
+
+def clean_profile():
+    '''
+    clean_profile: 
+        - prepares and cleans the Starbucks customer profile data for further processing
+
+    In:
+        - None
+    
+    Out:
+        - cleaned profile pandas dataframe
+    '''
+    # read in profile json file
+    profile = pd.read_json('data/profile.json', orient='records', lines=True)
+
+    # update column names
+    profile.rename(columns = {'id' : 'customer_id'}, inplace = True)
+
+    # remove the 2175 rows where "gender" and "income" have missing values and "age" equals 118
+    profile = profile[profile.age != 118]
+
+    # create membership_start column from became_member_on columns
+    profile['membership_start'] = pd.to_datetime(profile['became_member_on'].astype(str), format='%Y%m%d')
+
+    # create membership_year column from membership_start
+    profile['membership_year'] = pd.DatetimeIndex(profile['membership_start']).year
+
+    # one-hot encode 'gender' column
+    gender_temp = pd.get_dummies(profile['gender'])
+    gender_temp.rename(columns = {'F': 'female', 'M' : 'male', 'O' : 'other'}, inplace = True)
+
+    # merge gender_temp into profile
+    profile = pd.concat([profile, gender_temp], axis = 1)
+
+    # drop unneeded columns columns
+    profile.drop(['became_member_on', 'gender', 'other'], axis = 1, inplace = True)
+
+    # reorder columns
+    col_order = ['customer_id', 'age', 'income', 'female', 'male', 'membership_start', 'membership_year']
+
+    return profile.loc[:,col_order]
+
+def clean_transcript():
+    '''
+    clean_transcript:
+        - prepare and clean transcript data for further processing
+
+    In:
+        - None
+
+    Out:
+        - offers dataframe
+        - transactions dataframe 
+    '''
+    # read in json datafile
+    transcript = pd.read_json('data/transcript.json', orient='records', lines=True)
+
+    # rename 'person' column to 'customer_id'
+    transcript.rename(columns = {'person' : 'customer_id'}, inplace = True)
+
+    # one-hot encode 'event' column
+    event_temp = pd.get_dummies(transcript['event'])
+    event_temp.drop(['transaction'], axis = 1, inplace = True)
+
+    # merge event_temp into transcript
+    transcript = pd.concat([transcript, event_temp], axis = 1)
+
+    # create offers dataframe by pulling records where event is not equal 'transaction'
+    offers = transcript.query('event != "transaction"').copy()
+
+    # extract offer_id column from value column
+    offers['offer_id'] = offers['value'].apply(lambda x: list(x.values())[0])
+
+    # drop event and value
+    offers.drop(['event','value'], axis=1, inplace=True)
+
+    # rename columns
+    offers.rename(columns = {'offer completed' : 'offer_completed', 'offer received' : 'offer_received',
+                        'offer viewed' : 'offer_viewed'}, inplace = True)
+
+    # reorder offers columns
+    col_order = ['offer_id', 'customer_id', 'time', 'offer_completed', 'offer_received', 'offer_viewed']
+    
+    # create transcations dataframe by pulling records where event is equal to 'transaction'
+    transactions = transcript.query('event == "transaction"').copy()
+
+    # extract amount column from value column
+    transactions['amount'] = transactions['value'].apply(lambda x: list(x.values())[0])
+
+    # drop unneeded columns
+    transactions.drop(['event','value', 'offer completed', 'offer received', 'offer viewed'], axis=1, inplace=True)
+
+    return offers.loc[:, col_order], transactions
+
+def merge_data(portfolio, profile, offers, transactions):
+    '''
+    merge_data:
+        - merge the cleaned datasets
+    In:
+        - portfolio_clean - cleaned portfolio data
+        - profile_clean - cleaned profile data
+        - offers - clean transcripts data representing offfers
+        - transactions - cleaned transcript data representing transactions
+    Out:
+        - starbucks_offers_data.csv - merged offers data
+        - starbucks_transact_data.csv - merged transactions data
+    '''
+    # merge offers data and portfolio data
+    combined_offers_df = pd.merge(offers, portfolio, how = 'left', on = ['offer_id'])
+
+    # merge combined_offers with profile data
+    combined_offers_df = pd.merge(combined_offers_df, profile, how = 'left', on = ['customer_id'])
+
+    # merge transaction data with profile data
+    combined_transact_df = pd.merge(transactions, profile, how = 'left', on = ['customer_id'])
+
+    # save merged datasets as csv files
+    combined_offers_df.to_csv('data/starbucks_offers_data.csv', encoding = 'utf-8', index = False)
+    combined_transact_df.to_csv('data/starbucks_transact_data.csv', encoding = 'utf-8', index = False)
+
+    return
+
+def age_bins(df):
+    '''
+    age_bins:
+        - create age range bins
+    
+    In:
+        - dataframe with age column 
+    
+    Out:
+        - dataframe with age column transformed into age bins
+    '''
+    age_ranges = pd.cut(df['age'], bins = [18, 29, 44, 59, 74, 89, 101],
+                    labels = ['18-29', '30-44', '45-59', '60-74', '75-89', '90-101'])
+
+    # one-hot encode age_ranges
+    age_temp = pd.get_dummies(age_ranges)
+
+    # merge age columns into dataframe
+    df = pd.concat([df, age_temp], axis = 1)
+    
+    # drop unneeded age column
+    df.drop(['age'], axis = 1, inplace = True)
+
+    return df
+
+def income_bins(df):
+    '''
+    income_bins:
+        - create age range bins
+    
+    In:
+        - dataframe with income column 
+    
+    Out:
+        - dataframe with income column transformed into income bins
+    '''
+    income_ranges = pd.cut(df['income'], bins = [30000, 49000, 69000, 89000, 109000, 120000],
+                    labels = ['30k-49k', '50k-69k', '70k-89k', '90k-109k', '110k-120k'])
+
+    # one-hot encode age_ranges
+    income_temp = pd.get_dummies(income_ranges)
+
+    # merge age columns into dataframe
+    df = pd.concat([df, income_temp], axis = 1)
+    
+    # drop unneeded age column
+    df.drop(['income'], axis = 1, inplace = True)
+
+    return df
