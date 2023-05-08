3f66860c17bc78461418f36e96861dfb12be3705
mmascaro864 <marc.j.mascaro@gmail.com>
2023-05-08 15:26:46 -0400

    adding functions

--- recommender_functions.py
+++ recommender_functions.py
@@ -93,3 +93,84 @@ def find_similar_users(customer_id, user_item):
     most_similar_users.remove(customer_id)
        
     return most_similar_users # return a list of the users in order from most to least similar
+
+def get_offer_ids(offer_ids, df):
+    '''
+    INPUT:
+    offer_id - (list) a list of offer ids
+    df - (pandas dataframe) merged combined_df after cleaning original dataframes
+    
+    OUTPUT:
+    offer_ids - a list of Starbucks offer ids 
+    '''
+    # create empty list
+    offer_ids_list = []
+        
+    # loop to get offer_ids
+    for id in offer_ids_list:
+        offer_ids.append(df[df['offer_id'] == float(id)].iloc[0])
+    
+    return offer_ids # Return the offer_ids
+
+def get_user_offers(customer_id, user_item):
+    '''
+    INPUT:
+    user_id - (int) a user id
+    user_item - (pandas dataframe) matrix of customer_ids by offer_ids: 
+                1's when a user has interacted with an article, 0 otherwise
+    
+    OUTPUT:
+    offer_ids - (list) a list of the offer ids seen by the customer
+    
+    Description:
+    Provides a list of the offer_ids that have been seen by a customer
+    '''
+    offer_ids = list(user_item.loc[customer_id][user_item.loc[customer_id] == 1].index.astype(str))
+    
+    return offer_ids # return the ids and names
+
+def user_user_recs(customer_id, user_item_matrix, m = 10):
+    '''
+    INPUT:
+    customer_id - (int) a customer id
+    m - (int) the number of recommendations you want for the customer
+    
+    OUTPUT:
+    recs - (list) a list of recommendations for the customer
+    
+    Description:
+    Loops through the users based on closeness to the input customer_id
+    For each customer - finds offer_ids the customer hasn't seen before and provides them as recs
+    Does this until m recommendations are found
+    
+    Notes:
+    Customers who are the same closeness are chosen arbitrarily as the 'next' user
+    
+    For the customer where the number of recommended offers starts below m 
+    and ends exceeding m, the last items are chosen arbitrarily
+    
+    '''
+    # offers seen by customer
+    offers_seen = get_user_offers(customer_id, user_item_matrix)[0]
+    similar_users = find_similar_users(customer_id, user_item_matrix)
+    
+    # keep recommended offers here
+    recs = np.array([])
+    
+    # Go through the neighbors and identify offer_ids they like the customer hasn't seen
+    for user in similar_users:
+        user_likes = get_user_offers(user, user_item_matrix)[0]
+        
+        # Obtain recommendations for each neighbor
+        new_recs = np.setdiff1d(user_likes, offers_seen, assume_unique=True)
+        
+        # Update recs with new recs
+        recs = np.unique(np.concatenate([new_recs, recs], axis=0))
+        
+        # If we have enough recommendations exit the loop
+        if len(recs) > m-1:
+            break
+    
+    recs = recs[:m] 
+    
+    return recs # return your recommendations for this customer_id    
